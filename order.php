0d511bbdcbb02869a05dc981357bf426fadb3a63
stephanmaringka <52527428+stephanmaringka@users.noreply.github.com>
2020-01-26 02:57:39 +0800

    Update order.php

--- order.php
+++ order.php
@@ -7,7 +7,7 @@ include("functions/functions.php");
 
 ?>
 
-<?php
+<?php 
 
 if(isset($_GET['c_id'])){
     
@@ -37,11 +37,11 @@ while($row_cart = mysqli_fetch_array($run_cart)){
     
     $run_products = mysqli_query($con,$get_products);
     
-    while($row_product = mysqli_fetch_array($run_products)){
+    while($row_products = mysqli_fetch_array($run_products)){
         
         $sub_total = $row_products['product_price']*$pro_qty;
         
-        $insert_customer_order = "insert into customer_orders (customer_id,due_amount,invoice_no,qty,size,order_date,order_status) values ('$customer_id','$sub_total','$invoice_no','$pro_qty','$pro_size','NOW()','$status')";
+        $insert_customer_order = "insert into customer_orders (customer_id,due_amount,invoice_no,qty,size,order_date,order_status) values ('$customer_id','$sub_total','$invoice_no','$pro_qty','$pro_size',NOW(),'$status')";
         
         $run_customer_order = mysqli_query($con,$insert_customer_order);
         
