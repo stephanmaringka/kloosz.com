7a0296460e49dce34008f555334c965b4a2be565
stephanmaringka <52527428+stephanmaringka@users.noreply.github.com>
2020-01-26 00:17:53 +0800

    Update shop.php

--- shop.php
+++ shop.php
@@ -1,9 +1,10 @@
 <?php 
-    
+
     $active='Shop';
     include("includes/header.php");
 
 ?>
+
 <div id="content">
     <div class="container">
         <div class="col-md-12">
@@ -13,22 +14,16 @@
             </ul>
         </div>
         <div class="col-md-3">
-            <?php 
+           
+        <?php 
     
-                include("includes/sidebar.php");
+        include("includes/sidebar.php");
     
-                ?>
+        ?>
+        
         </div>
 
         <div class="col-md-9">
-
-            <?php 
-               
-                if(!isset($_GET['p_cat'])){
-                    
-                    if(!isset($_GET['cat'])){
-              
-                      echo "
 
                        <div class='box'>
                            <h1>Shop</h1>
@@ -37,175 +32,23 @@
                            </p>
                        </div>
 
-                       ";
-                        
-                    }
-                   
-                   }
-               
-               ?>
+            <div id="products" class="row">
 
-            <div class="row">
-
-                <?php 
-                   
-                        if(!isset($_GET['p_cat'])){
-                            
-                         if(!isset($_GET['cat'])){
-                            
-                            $per_page=6; 
-                             
-                            if(isset($_GET['page'])){
-                                
-                                $page = $_GET['page'];
-                                
-                            }else{
-                                
-                                $page=1;
-                                
-                            }
-                            
-                            $start_from = ($page-1) * $per_page;
-                             
-                            $get_products = "select * from products order by 1 DESC LIMIT $start_from,$per_page";
-                             
-                            $run_products = mysqli_query($con,$get_products);
-                             
-                            while($row_products=mysqli_fetch_array($run_products)){
-                                
-                                $pro_id = $row_products['product_id'];
-        
-                                $pro_title = $row_products['product_title'];
-
-                                $pro_price = $row_products['product_price'];
-
-                                $pro_img1 = $row_products['product_img1'];
-                                
-                                echo "
-                                
-                                    <div class='col-md-4 col-sm-6 center-responsive'>
-                                    
-                                        <div class='product'>
-                                        
-                                            <a href='details.php?pro_id=$pro_id'>
-                                            
-                                                <img class='img-responsive' src='admin_area/product_images/$pro_img1'>
-                                            
-                                            </a>
-                                            
-                                            <div class='text'>
-                                            
-                                                <h3>
-                                                
-                                                    <a href='details.php?pro_id=$pro_id'> $pro_title </a>
-                                                
-                                                </h3>
-                                            
-                                                <p class='price'>
-
-                                                    Rp $pro_price
-
-                                                </p>
-
-                                                <p class='buttons'>
-
-                                                    <a class='btn btn-default' href='details.php?pro_id=$pro_id'>
-
-                                                        View Details
-
-                                                    </a>
-
-                                                    <a class='btn btn-primary' href='details.php?pro_id=$pro_id'>
-
-                                                        <i class='fa fa-shopping-cart'></i> Add To Cart
-
-                                                    </a>
-
-                                                </p>
-                                            
-                                            </div>
-                                        
-                                        </div>
-                                    
-                                    </div>
-                                
-                                ";
-                                
-                        }
-                        
-                   ?>
+            <?php getProducts(); ?>
 
             </div>
             <center>
                 <ul class="pagination">
-
-                    <?php
-                             
-                    $query = "select * from products";
-                             
-                    $result = mysqli_query($con,$query);
-                             
-                    $total_records = mysqli_num_rows($result);
-                             
-                    $total_pages = ceil($total_records / $per_page);
-                             
-                        echo "
-                        
-                            <li>
-                            
-                                <a href='shop.php?page=1'> ".'First Page'." </a>
-                            
-                            </li>
-                        
-                        ";
-                             
-                        for($i=1; $i<=$total_pages; $i++){
-                            
-                              echo "
-                        
-                            <li>
-                            
-                                <a href='shop.php?page=".$i."'> ".$i." </a>
-                            
-                            </li>
-                        
-                        ";  
-                            
-                        };
-                             
-                        echo "
-                        
-                            <li>
-                            
-                                <a href='shop.php?page=$total_pages'> ".'Last Page'." </a>
-                            
-                            </li>
-                        
-                        ";
-                             
-					    	}
-							
-						}
-					 
-					 ?>
-
+                
+                    <?php getPaginator(); ?>
+                
                 </ul>
             </center>
             
-            <div class="row">
-                
-                <?php
-            
-                getpcatpro();
-                
-                getcatpro();
-
-                ?>  
-                
-            </div>
-            
             
         </div>
+        
+        <div id="wait" class="wait" style="position:absolute;top:40%;left:45%;padding: 200px 100px 100px 100px"></div>
 
     </div>
 </div>
@@ -221,6 +64,79 @@
 
 <script src="js/jquery-331.min.js"></script>
 <script src="js/bootstrap-337.min.js"></script>
+<script>
+
+   $(document).ready(function(){
+      
+       // Hide & Show Sidebar Toggle //
+       
+       $('.nav-toggle').click(function(){
+           
+           $('.panel-collapse,.collapse-data').slideToggle(700,function(){
+               
+               if($(this).css('display')=='none'){
+                   
+                   $(".hide-show").html('Show');
+                   
+               }else{
+                   
+                   $(".hide-show").html('Hide');
+                   
+               }
+               
+           });
+           
+       });
+       
+       // Search Filters by letter //
+       
+        $(function(){
+            
+           $.fn.extend({
+               
+              filterTable: function(){
+                  
+                  return this.each(function(){
+                      
+                     $(this).on('keyup', function(){
+                         
+                        var $this = $(this), 
+                        search = $this.val().toLowerCase(), 
+                        target = $this.attr('data-filters'),
+                        handle = $(target),
+                        rows = handle.find('li a');
+                        
+                        if(search == ''){
+                            
+                            rows.show();
+                            
+                        }else{
+                            
+                            rows.each(function(){
+                                
+                                var $this = $(this);
+                                
+                                $this.text().toLowerCase().indexOf(search) === -1? $this.hide() : $this.show();
+                                
+                            });
+                            
+                        }
+                        
+                     }); 
+                      
+                  });
+                  
+              } 
+               
+           });
+            
+            $('[data-action="filter"][id="dev-table-filter"]').filterTable();
+            
+        });
+       
+       });
+
+</script>
 
 
 </body>
