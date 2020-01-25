a5c6f6a423ef74895f627181a92d833d63f252b1
stephanmaringka <52527428+stephanmaringka@users.noreply.github.com>
2020-01-25 16:47:44 +0800

    Update edit_cat.php

--- admin_area/edit_cat.php
+++ admin_area/edit_cat.php
@@ -36,9 +36,9 @@
     <div class="col-lg-12">
         <ol class="breadcrumb">
             <li>
-
+                
                 <i class="fa fa-dashboard"></i> Dashboard / Edit Category
-
+                
             </li>
         </ol>
     </div>
@@ -49,94 +49,103 @@
         <div class="panel panel-default">
             <div class="panel-heading">
                 <h3 class="panel-title">
-
+                
                     <i class="fa fa-pencil fa-fw"></i> Edit Category
-
+                
                 </h3>
             </div>
-
+            
             <div class="panel-body">
-
                 <form action="" class="form-horizontal" method="post" enctype="multipart/form-data">
                     <div class="form-group">
-
+                    
                         <label for="" class="control-label col-md-3">
-
-                            Category Title
-
+                        
+                            Category Title 
+                        
                         </label>
-
+                        
                         <div class="col-md-6">
-
-                            <input value="<?php echo $cat_title ?>" name="cat_title" type="text" class="form-control">
-
+                        
+                            <input value=" <?php echo $cat_title; ?> " name="cat_title" type="text" class="form-control">
+                        
                         </div>
-
+                    
                     </div>
-
+                    
                     <div class="form-group">
-
+                    
                         <label for="" class="control-label col-md-3">
-
+                        
                             Choose as Top Manufacturer
-
+                        
                         </label>
-
+                        
                         <div class="col-md-6">
-
-                            <input name="cat_top" type="radio" value="yes" <?php
+                        
+                            <input name="cat_top" type="radio" value="yes"
+                            
+                                <?php
                                 
                                     if($cat_top=='no'){}else{echo "checked='checked'";}
         
-                                ?>>
+                                ?>
+                            
+                            >
                             <label>Yes</label>
-
-                            <input name="cat_top" type="radio" value="no" <?php 
+                            
+                            <input name="cat_top" type="radio" value="no"
+                            
+                                <?php 
                                    
                                     if($cat_top=='no'){echo "checked='checked'";}   
                                    
-                                ?>>
-                            <label>No</label>
+                                ?>
+                                
+                            >
+                            <label>No</label>    
                         </div>
-
+                    
                     </div>
-
+                      
                     <div class="form-group">
-
+                    
                         <label for="" class="control-label col-md-3">
-
+                        
                             Category Image
-
+                        
                         </label>
-
+                        
                         <div class="col-md-6">
-
+                        
                             <input type="file" name="cat_image" class="form-control">
-
+                            
                             <br>
-
-                            <img width="70" height="70" src="other_images/<?php echo $cat_image; ?>" alt="<?php echo $cat_image; ?>">
-
+                          
+                          <img width="70" height="70" src="other_images/<?php echo $cat_image; ?>" alt="<?php echo $cat_image; ?>">
+                        
                         </div>
-
+                    
                     </div>
-
+                        
                     <div class="form-group">
-
-                        <label for="" class="control-label col-md-3">
-
+                    
+                        <label for="" class="control-label col-md-3"> 
+                        
+                             
+                        
                         </label>
-
+                        
                         <div class="col-md-6">
-
-                            <input value="Update Category" name="udpate" type="submit" class="form-control btn btn-primary">
-
+                        
+                            <input value="Update Category" name="update" type="submit" class="form-control btn btn-primary">
+                        
                         </div>
-
+                    
                     </div>
                 </form>
             </div>
-
+            
         </div>
     </div>
 </div>
@@ -150,43 +159,42 @@
               $cat_top = $_POST['cat_top'];
               
               if(is_uploaded_file($_FILES['cat_image']['tmp_name'])){
-                  
-                  $cat_image = $_POST['cat_image']['name'];
+                 
               
-                  $temp_name = $_FILES['cat_image']['tmp_name'];
-                  
-                  //move_uploaded_file($temp_name,"other_images/$cat_image");
+              $cat_image = $_FILES['cat_image']['name'];
               
-                  $update_cat = "update categories set cat_title='$cat_title',cat_top='$cat_top',cat_image='$cat_image' where cat_id='$cat_id'";
+              $temp_name = $_FILES['cat_image']['tmp_name'];
               
-                  $run_cat = mysqli_query($con,$update_cat);
-                  
-                  if($run_cat){
-                      
-                      echo "<script>alert('Your category has been updated')</script>";
-                          
-                      echo "<script>window.open('index.php?view_cats','_self')</script>";
-                      
-                  }
-                  
-              }else{
-                  
-                 $update_cat = "update categories set cat_title='$cat_title',cat_top='$cat_top' where cat_id='$cat_id'";
+              $update_cat = "update categories set cat_title='$cat_title',cat_top='$cat_top',cat_image='$cat_image' where cat_id='$cat_id'";
               
-                  $run_cat = mysqli_query($con,$update_cat);
+              $run_cat = mysqli_query($con,$update_cat);
+              
+              if($run_cat){
                   
-                  if($run_cat){
-                      
-                      echo "<script>alert('Your category has been updated')</script>";
-                          
-                      echo "<script>window.open('index.php?view_cats','_self')</script>";
-                      
-                  } 
+                  echo "<script>alert('Your Category Has Been Updated')</script>";
+                  
+                  echo "<script>window.open('index.php?view_cats','_self')</script>";
                   
               }
+                  
+              }else{
+                 $update_cat = "update categories set cat_title='$cat_title',cat_top='$cat_top' where cat_id='$cat_id'";
               
-            }
+              $run_cat = mysqli_query($con,$update_cat);
+              
+              if($run_cat){
+                  
+                  echo "<script>alert('Your Category Has Been Updated')</script>";
+                  
+                  echo "<script>window.open('index.php?view_cats','_self')</script>";
+                  
+              } 
+              }
+              
+          }
 
 ?>
 
-<?php } ?>
+
+
+<?php } ?> 
