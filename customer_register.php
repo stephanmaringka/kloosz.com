91c9b7354f947ebe877a8d30e53555268941e021
stephanmaringka <52527428+stephanmaringka@users.noreply.github.com>
2020-01-26 17:41:43 +0800

    Update customer_register.php

--- customer_register.php
+++ customer_register.php
@@ -5,6 +5,7 @@
 
 ?>
 
+<!-- content -->
 <div id="content">
     <div class="container">
         <div class="col-md-12">
@@ -39,44 +40,72 @@
                             If you have any questions. feel free to contact us. Our Customer Service work <strong>24/7</strong>
                         </p>
                     </center>
+                    
                     <form action="customer_register.php" method="post" enctype="multipart/form-data">
+                       
+                        <!-- form group register -->
                         <div class="form-group">
                             <label>Your Name</label>
                             <input type="text" class="form-control" name="c_name" required>
                         </div>
+                        <!-- form group register end -->
+                        
+                        <!-- form group email -->
                         <div class="form-group">
                             <label>Your Email</label>
                             <input type="text" class="form-control" name="c_email" required>
                         </div>
+                        <!-- form group email end -->
+                        
+                        <!-- form group password -->
                         <div class="form-group">
                             <label>Your Password</label>
                             <input type="password" class="form-control" name="c_pass" required>
                         </div>
+                        <!-- form group password end -->
+                        
+                        <!-- form group country -->
                         <div class="form-group">
                             <label>Your Country</label>
                             <input type="text" class="form-control" name="c_country" required>
                         </div>
+                        <!-- form group country end -->
+                        
+                        <!-- form group city -->
                         <div class="form-group">
                             <label>Your City</label>
                             <input type="text" class="form-control" name="c_city" required>
                         </div>
+                        <!-- form group city end -->
+                        
+                        <!-- form group contact -->
                         <div class="form-group">
                             <label>Your Contact</label>
                             <input type="text" class="form-control" name="c_contact" required>
                         </div>
+                        <!-- form group contact end -->
+                        
+                        <!-- form group address -->
                         <div class="form-group">
                             <label>Your Address</label>
                             <input type="text" class="form-control" name="c_address" required>
                         </div>
+                        <!-- form group address end -->
+                        
+                        <!-- form group profile picture -->
                         <div class="form-group">
                             <label>Your Profile Picture</label>
                             <input type="file" class="form-control form-height-custom" name="c_image" required>
                         </div>
+                        <!-- form group profile picture end -->
+                        
+                        <!-- register submit -->
                         <div class="text-center">
                             <button type="submit" name="register" class="btn btn-primary">
                                 <i class="fa fa-user-md"></i> Register
                             </button>
                         </div>
+                        <!-- register submit end -->
                     </form>
                 </div>
             </div>
@@ -84,8 +113,7 @@
 
     </div>
 </div>
-
-
+<!-- content end -->
 
 <?php 
     
