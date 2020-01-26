5a0bb4877af01675ecf869e06b6839cede88ab97
stephanmaringka <52527428+stephanmaringka@users.noreply.github.com>
2020-01-26 17:47:19 +0800

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -4,35 +4,35 @@
     include("includes/header.php");
 
 ?>
-   
-   <div id="content">
-       <div class="container">
-           <div class="col-md-12">
-               
-               <ul class="breadcrumb">
-                   <li>
-                       <a href="index.php">Home</a>
-                   </li>
-                   <li>
-                       Register
-                   </li>
-               </ul>
-               
-           </div>
-           
-           <div class="col-md-3">
-   
-   <?php 
+
+<div id="content">
+    <div class="container">
+        <div class="col-md-12">
+
+            <ul class="breadcrumb">
+                <li>
+                    <a href="index.php">Home</a>
+                </li>
+                <li>
+                    Register
+                </li>
+            </ul>
+
+        </div>
+
+        <div class="col-md-3">
+
+            <?php 
     
     include("includes/sidebar.php");
     
     ?>
-               
-           </div>
-           
-           <div class="col-md-9">
-           
-           <?php 
+
+        </div>
+
+        <div class="col-md-9">
+
+            <?php 
            
            if(!isset($_SESSION['customer_email'])){
                
@@ -45,87 +45,25 @@
            }
            
            ?>
-           
-           </div>
-           
-       </div>
-   </div>
-   
-   <?php 
+
+        </div>
+
+    </div>
+</div>
+<!-- Content End -->
+
+    <?php 
     
     include("includes/footer.php");
     
     ?>
 
-    <!-- Content End -->
 
 
-    <script src="js/jquery-331.min.js"></script>
-    <script src="js/bootstrap-337.min.js"></script>
+
+<script src="js/jquery-331.min.js"></script>
+<script src="js/bootstrap-337.min.js"></script>
 
 
 </body>
 </html>
-
-
-<?php 
-
-if(isset($_POST['register'])){
-    
-    $c_name = $_POST['c_name'];
-    
-    $c_email = $_POST['c_email'];
-    
-    $c_pass = $_POST['c_pass'];
-    
-    $c_country = $_POST['c_country'];
-    
-    $c_city = $_POST['c_city'];
-    
-    $c_contact = $_POST['c_contact'];
-    
-    $c_address = $_POST['c_address'];
-    
-    $c_image = $_FILES['c_image']['name'];
-    
-    $c_image_tmp = $_FILES['c_image']['tmp_name'];
-    
-    $c_ip = getRealIpUser();
-    
-    move_uploaded_file($c_image_tmp,"customer/customer_images/$c_image");
-    
-    $insert_customer = "insert into customers (customer_name,customer_email,customer_pass,customer_country,customer_city,customer_contact,customer_address,customer_image,customer_ip) values ('$c_name','$c_email','$c_pass','$c_country','$c_city','$c_contact','$c_address','$c_image','$c_ip')";
-    
-    $run_customer = mysqli_query($con,$insert_customer);
-    
-    $sel_cart = "select * from cart where ip_add='$c_ip'";
-    
-    $run_cart = mysqli_query($con,$sel_cart);
-    
-    $check_cart = mysqli_num_rows($run_cart);
-    
-    if($check_cart>0){
-        
-        // register ada item di cart //
-        
-        $_SESSION['customer_email']=$c_email;
-        
-        echo "<script>alert('Registration Successful')</script>";
-        
-        echo "<script>window.open('checkout.php','_self')</script>";
-        
-    }else{
-        
-        // register tanpa item di cart //
-        
-        $_SESSION['customer_email']=$c_email;
-        
-        echo "<script>alert('Registration Successful')</script>";
-        
-        echo "<script>window.open('index.php','_self')</script>";
-        
-    }
-    
-}
-
-?>
