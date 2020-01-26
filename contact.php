cdc8864648e5d5e2d6205ebf4a98026de2dddbec
stephanmaringka <52527428+stephanmaringka@users.noreply.github.com>
2020-01-26 17:45:30 +0800

    Update contact.php

--- contact.php
+++ contact.php
@@ -5,62 +5,63 @@
 
 ?>
 
-    <div id="content">
-        <div class="container">
-            <div class="col-md-12">
-                <ul class="breadcrumb">
-                    <li><a href="index.php">Home</a></li>
-                    <li>Contact Us</li>
-                </ul>
-            </div>
-            <div class="col-md-3">
+<!-- content -->
+<div id="content">
+    <div class="container">
+        <div class="col-md-12">
+            <ul class="breadcrumb">
+                <li><a href="index.php">Home</a></li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div class="col-md-3">
 
 
-                <?php 
+            <?php 
     
                 include("includes/sidebar.php");
     
                 ?>
-            </div>
+        </div>
 
-            <div class="col-md-9">
-                <div class="box">
-                    <div class="box-header">
-                        <center>
-                            <h2> Feel Free to Contach Us</h2>
-                            <p class="text-muted">
-                                If you have any questions. feel free to contact us. Our Customer Service work <strong>24/7</strong>
-                            </p>
-                        </center>
-                        <form action="contact.php" method="post">
-                            <div class="form-group">
-                                <label>Name</label>
-                                <input type="text" class="form-control" name="name" required>
-                            </div>
-                            <div class="form-group">
-                                <label>Email</label>
-                                <input type="text" class="form-control" name="email" required>
-                            </div>
-                            <div class="form-group">
-                                <label>Subject</label>
-                                <input type="text" class="form-control" name="subject" required>
-                            </div>
-                            <div class="form-group">
-                                <label>Message</label>
-                                <textarea name="message" class="form-control"></textarea>
-                            </div>
-                            <div class="text-center">
-                                <button type="submit" name="submit" class="btn btn-primary">
-                                    <i class="fa fa-user-md"></i> Send Message
-                                </button>
-                            </div>
-                        </form>
-                        
-                        <?php
+        <div class="col-md-9">
+            <div class="box">
+                <div class="box-header">
+                    <center>
+                        <h2> Feel Free to Contach Us</h2>
+                        <p class="text-muted">
+                            If you have any questions. feel free to contact us. Our Customer Service work <strong>24/7</strong>
+                        </p>
+                    </center>
+                    <form action="contact.php" method="post">
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" class="form-control" name="name" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Email</label>
+                            <input type="text" class="form-control" name="email" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Subject</label>
+                            <input type="text" class="form-control" name="subject" required>
+                        </div>
+                        <div class="form-group">
+                            <label>Message</label>
+                            <textarea name="message" class="form-control"></textarea>
+                        </div>
+                        <div class="text-center">
+                            <button type="submit" name="submit" class="btn btn-primary">
+                                <i class="fa fa-user-md"></i> Send Message
+                            </button>
+                        </div>
+                    </form>
+
+                    <?php
                         
                         if(isset($_POST['submit'])){
                             
-                            // Admin dapat message disini //
+                            // Admin receives messages //
                             
                             $sender_name = $_POST['name'];
                            
@@ -70,7 +71,7 @@
                            
                             $sender_message = $_POST['message'];
                             
-                            $receiver_email = "hasnelindonesia@gmail.com";
+                            $receiver_email = "admin@kloosz.com";
                             
                             mail($receiver_email,$sender_name,$sender_subject,$sender_message,$sender_email);
                             
@@ -91,28 +92,24 @@
                         }
                        
                         ?>
-                        
-                    </div>
+
                 </div>
             </div>
-
         </div>
+
     </div>
+</div>
+<!-- content end -->
 
-
-
-    <?php 
+<?php 
     
     include("includes/footer.php");
     
     ?>
 
-    <!-- Content End -->
-
-
-    <script src="js/jquery-331.min.js"></script>
-    <script src="js/bootstrap-337.min.js"></script>
-
+<script src="js/jquery-331.min.js"></script>
+<script src="js/bootstrap-337.min.js"></script>
 
 </body>
+
 </html>
