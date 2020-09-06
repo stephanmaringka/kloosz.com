ba0e82097ef32c16e4e13638cb7590f5533ae3ec
stephanmaringka <52527428+stephanmaringka@users.noreply.github.com>
2020-09-06 22:43:43 +0800

    Update order.php

--- order.php
+++ order.php
@@ -1,7 +1,5 @@
 <?php
 
-session_start();
-
 include("includes/db.php");
 include("functions/functions.php");
 
@@ -32,33 +30,26 @@ while($row_cart = mysqli_fetch_array($run_cart)){
     $pro_qty = $row_cart['qty'];
     
     $pro_size = $row_cart['size'];
-    
-    $get_products = "select * from products where product_id='$pro_id'";
-    
-    $run_products = mysqli_query($con,$get_products);
-    
-    while($row_products = mysqli_fetch_array($run_products)){
         
-        $sub_total = $row_products['product_price']*$pro_qty;
+    $sub_total = $row_cart['p_price']*$pro_qty;
         
-        $insert_customer_order = "insert into customer_orders (customer_id,due_amount,invoice_no,qty,size,order_date,order_status) values ('$customer_id','$sub_total','$invoice_no','$pro_qty','$pro_size',NOW(),'$status')";
+    $insert_customer_order = "insert into customer_orders (customer_id,due_amount,invoice_no,qty,size,order_date,order_status) values ('$customer_id','$sub_total','$invoice_no','$pro_qty','$pro_size',NOW(),'$status')";
         
-        $run_customer_order = mysqli_query($con,$insert_customer_order);
+    $run_customer_order = mysqli_query($con,$insert_customer_order);
         
-        $insert_pending_order = "insert into pending_orders (customer_id,invoice_no,product_id,qty,size,order_status) values ('$customer_id','$invoice_no','$pro_id','$pro_qty','$pro_size','$status')";
+    $insert_pending_order = "insert into pending_orders (customer_id,invoice_no,product_id,qty,size,order_status) values ('$customer_id','$invoice_no','$pro_id','$pro_qty','$pro_size','$status')";
         
-        $run_pending_order = mysqli_query($con,$insert_pending_order);
+    $run_pending_order = mysqli_query($con,$insert_pending_order);
         
-        $delete_cart = "delete from cart where ip_add='$ip_add'";
+    $delete_cart = "delete from cart where ip_add='$ip_add'";
         
-        $run_delete = mysqli_query($con,$delete_cart);
+    $run_delete = mysqli_query($con,$delete_cart);
         
-        echo "<script>alert('Pesanan Anda berhasil, Terima kasih')</script>";
+    echo "<script>alert('Pesanan Anda berhasil, Terima kasih')</script>";
         
-        echo "<script>window.open('customer/my_account.php?my_orders','_self')</script>";
+    echo "<script>window.open('customer/my_account.php?my_orders','_self')</script>";
         
-    }
-    
+       
 }
 
 ?>
